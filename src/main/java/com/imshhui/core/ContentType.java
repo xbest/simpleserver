d6879d5309fe3b1d6c27ff727388b8c51444d8f3
liyulin <liyulin@xiaomi.com>
2020-07-30 11:23:59 +0800

    guess type

--- src/main/java/com/imshhui/core/ContentType.java
+++ src/main/java/com/imshhui/core/ContentType.java
@@ -9,4 +9,14 @@ public interface ContentType {
     String IMAGE_JPEG = "Content-Type: image/jpeg";
     String OCTET_STREAM = "Content-Type: application/octet-stream";
     String TEXT_PLAIN = "Content-Type: text/plain; charset=UTF-8";
+
+    static String guessType(String filePath) {
+        if (filePath.endsWith("jpg") || filePath.endsWith("jpeg")) {
+            return IMAGE_JPEG;
+        } else if (filePath.endsWith("zip")) {
+            return OCTET_STREAM;
+        } else {
+            return TEXT_PLAIN;
+        }
+    }
 }
--- src/main/java/com/imshhui/core/RequestHandler.java
+++ src/main/java/com/imshhui/core/RequestHandler.java
@@ -33,47 +33,13 @@ public class RequestHandler implements Runnable {
             String filePath = SimpleHttpServer.BASE_PATH + uri;
 
             File file = new File(filePath);
-
             if (file.isDirectory()) {
-                StringBuilder sb = new StringBuilder();
-                String path = file.getPath();
-                sb.append("<!DOCTYPE html>\r\n");
-                sb.append("<html><head><title>");
-                sb.append("Listing of: ");
-                sb.append(path);
-                sb.append("</title></head><body>\r\n");
-                sb.append("<h2>Directory listing for ");
-                sb.append(path);
-                sb.append("</h2>\r\n");
-                sb.append("<hr>");
-                sb.append("<ul>");
-                sb.append("<li><a href=\"..");
-                sb.append("/".equals(uri) ? "/" : uri.substring(0, uri.lastIndexOf("/")));
-                sb.append("\">..</a></li>\r\n");
-                for (File f : file.listFiles()) {
-                    if (!f.canRead()) {
-                        continue;
-                    }
-                    String name = f.getName();
-                    sb.append("<li><a href=\"..");
-                    sb.append("/".equals(uri) ? "" : uri);
-                    sb.append("\\" + name);
-                    sb.append("\">");
-                    sb.append(name);
-                    sb.append("</a></li>\r\n");
-                }
-                sb.append("</ul></body></html>\r\n");
-                sb.append("<hr>\r\n");
-                sb.append("\r\n");
-                responseBuilder.contentType(ContentType.TEXT_HTML).content(sb.toString().getBytes());
-
-            } else if (filePath.endsWith("jpg") || filePath.endsWith("jpeg")) {
-                responseBuilder.contentType(ContentType.IMAGE_JPEG).content(Files.readAllBytes(file.toPath()));
-            } else if (filePath.endsWith("zip")) {
-                responseBuilder.contentType(ContentType.OCTET_STREAM).content(Files.readAllBytes(file.toPath()));
+                String content = directoryListing(uri, file);
+                responseBuilder.contentType(ContentType.TEXT_HTML).content(content.getBytes());
             } else {
-                responseBuilder.contentType(ContentType.TEXT_PLAIN).content(Files.readAllBytes(file.toPath()));
+                responseBuilder.contentType(ContentType.guessType(filePath)).content(Files.readAllBytes(file.toPath()));
             }
+
             responseBuilder.build().send();
         } catch (Exception e) {
             try {
@@ -84,8 +50,43 @@ public class RequestHandler implements Runnable {
                 e1.printStackTrace();
             }
         } finally {
-            close(reader);
+            close(reader, outputStream);
         }
+    }
+
+
+    private String directoryListing(String uri, File file) {
+        StringBuilder sb = new StringBuilder();
+        String path = file.getPath();
+        sb.append("<!DOCTYPE html>\r\n");
+        sb.append("<html><head><title>");
+        sb.append("Listing of: ");
+        sb.append(path);
+        sb.append("</title></head><body>\r\n");
+        sb.append("<h2>Directory listing for ");
+        sb.append(path);
+        sb.append("</h2>\r\n");
+        sb.append("<hr>");
+        sb.append("<ul>");
+        sb.append("<li><a href=\"..");
+        sb.append("/".equals(uri) ? "/" : uri.substring(0, uri.lastIndexOf("/")));
+        sb.append("\">..</a></li>\r\n");
+        for (File f : file.listFiles()) {
+            if (!f.canRead()) {
+                continue;
+            }
+            String name = f.getName();
+            sb.append("<li><a href=\"..");
+            sb.append("/".equals(uri) ? "" : uri);
+            sb.append("\\" + name);
+            sb.append("\">");
+            sb.append(name);
+            sb.append("</a></li>\r\n");
+        }
+        sb.append("</ul></body></html>\r\n");
+        sb.append("<hr>\r\n");
+        sb.append("\r\n");
+        return sb.toString();
     }
 
     private void close(Closeable... closeables) {
